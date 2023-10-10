77e171a23f11c8036de5d8ebd3ea81bf6ebf6918
Fabian Mendez <ofmendez@gmail.com>
2023-10-09 23:04:12 -0500

    Version 1 funcional

--- app/src/main/java/com/example/wifitile/MainActivity.kt
+++ app/src/main/java/com/example/wifitile/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.wifitile
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
--- app/src/main/java/com/example/wifitile/TileService.kt
+++ app/src/main/java/com/example/wifitile/TileService.kt
@@ -1,12 +1,26 @@
 package com.example.wifitile
-
+import android.app.PendingIntent
+import android.content.Intent
+import android.net.wifi.WifiManager
+import android.provider.Settings
+import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat.startActivityForResult
+
 
 class MyTileService: TileService(){
-
+    private var clicks = 0
+    @RequiresApi(34)
     override fun onClick() {
         super.onClick()
+        clicks++
 
+        qsTile.label =" ${getString(R.string.of_wifi_tile_name)} $clicks"
+        val panelIntent = Intent(Settings.Panel.ACTION_WIFI)
+        startActivityAndCollapse(PendingIntent.getActivity(this, 0, panelIntent,  PendingIntent.FLAG_IMMUTABLE))
+        //startActivity( Intent(WifiManager.ACTION_PICK_WIFI_NETWORK))
+        qsTile.updateTile()
         // Called when the user click the tile
     }
 
@@ -24,13 +38,23 @@ class MyTileService: TileService(){
 
     override fun onStartListening() {
         super.onStartListening()
+        val wifiManager = this.getSystemService(WIFI_SERVICE) as WifiManager
+        val state = !wifiManager.isWifiEnabled
 
-        // Called when the Tile becomes visible
+        qsTile.state = if (state) Tile.STATE_INACTIVE else Tile.STATE_ACTIVE
+
+        qsTile.updateTile()
     }
 
     override fun onStopListening() {
         super.onStopListening()
+        super.onStartListening()
+        val wifiManager = this.getSystemService(WIFI_SERVICE) as WifiManager
+        val state = !wifiManager.isWifiEnabled
 
+        qsTile.state = if (state) Tile.STATE_INACTIVE else Tile.STATE_ACTIVE
+
+        qsTile.updateTile()
         // Called when the tile is no longer visible
     }
 }
